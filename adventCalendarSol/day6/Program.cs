92fe2bac078155cebd27f43a557150b3021342dc
mpuigdomenech <miquel.puigdomenech@sage.com>
2021-12-26 18:58:31 +0100

    added first solution 6

---
+++ adventCalendarSol/day6/Program.cs
@@ -0,0 +1,31 @@
+﻿
+var list = (await File.ReadAllLinesAsync(@"input.txt"))
+            .First()
+            .Split(",")
+            .Select(x => int.TryParse(x, out var res) ? res : 0)
+            .ToList();
+
+
+foreach (var line in Enumerable.Range(1,80))
+{
+    var newCh = new List<int>();
+    for (int i = 0; i < list.Count; i++)
+    {
+        if (list[i] == 0)
+        {
+            newCh.Add(8);
+            list[i] = 6 ;
+        }
+        else
+        {
+            list[i]--;
+        }
+    }
+    list.AddRange(newCh);
+}
+
+Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>");
+Console.WriteLine($"count => {list.Count}");
+Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>");
+
+// Functions 
