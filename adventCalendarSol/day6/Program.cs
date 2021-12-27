f33bb4b5012d5ba1b3740dd48f4e02ce17a90a31
mpuigdomenech <miquel.puigdomenech@sage.com>
2021-12-27 11:11:12 +0100

    new impl using boxes and moving counts arround doh !

--- adventCalendarSol/day6/Program.cs
+++ adventCalendarSol/day6/Program.cs
@@ -6,26 +6,38 @@ var list = (await File.ReadAllLinesAsync(@"input.txt"))
             .ToList();
 
 
-foreach (var line in Enumerable.Range(1,80))
+var lanternFishSep = new long[9] {0, 0 , 0 , 0, 0, 0, 0, 0, 0};
+
+foreach (var item in list)
 {
-    var newCh = new List<int>();
-    for (int i = 0; i < list.Count; i++)
-    {
-        if (list[i] == 0)
-        {
-            newCh.Add(8);
-            list[i] = 6 ;
-        }
-        else
-        {
-            list[i]--;
-        }
-    }
-    list.AddRange(newCh);
+    lanternFishSep[item] += 1;
 }
 
+//printArrayLantern(0, lanternFishSep);
+
+foreach (var line in Enumerable.Range(1,256))
+{
+    var lanternFishSepCp = new long[9];
+
+    Array.Copy(lanternFishSep, 1, lanternFishSepCp, 0, lanternFishSep.Length - 1);
+    lanternFishSepCp[8] = lanternFishSep[0];
+    lanternFishSepCp[6] = lanternFishSepCp[6] + lanternFishSep[0];
+
+    lanternFishSep = lanternFishSepCp;
+
+    //printArrayLantern(line, lanternFishSep);
+}
+
+
 Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>");
-Console.WriteLine($"count => {list.Count}");
+Console.WriteLine($"count => {lanternFishSep.ToList().Sum()}");
 Console.WriteLine($">>>>>>>>>>>>>>>>>>>>>");
 
+
 // Functions 
+void printArrayLantern(int iteration, long[] lo)
+{
+    Console.WriteLine($"lanternFish Day {iteration}");
+    lo.ToList().ForEach(l => Console.Write($"{l},"));
+    Console.WriteLine(Environment.NewLine);
+}
